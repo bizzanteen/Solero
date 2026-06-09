b9ab8308fe496faf399de5b4cbfc68600b7a1527
Eamon <eamonc123@gmail.com>
2026-06-09 12:24:18 +0700

    RadiumPrep - generate fake-mo2 + merge-write settings.json

---
+++ src/tools/RadiumPrep.cpp
@@ -0,0 +1,81 @@
+#include "RadiumPrep.h"
+#include "core/Profile.h"
+#include "core/PluginList.h"
+#include "core/Types.h"
+#include "core/FileUtil.h"
+#include <QDir>
+#include <QFile>
+#include <QFileInfo>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QStringList>
+
+namespace solero {
+namespace RadiumPrep {
+
+QString defaultSettingsPath() {
+    return QDir::homePath() + "/.config/radium-textures/settings.json";
+}
+
+bool prepare(const Profile& profile,
+             const QString& gameDir,
+             const QString& installDir,
+             const QString& outputDataDir,
+             const QString& settingsPath,
+             QString* error) {
+    auto fail = [&](const QString& m) { if (error) *error = m; return false; };
+
+    const QString fakeMo2 = installDir + "/fake-mo2";
+    const QString modsDir  = fakeMo2 + "/mods";
+    const QString profDir  = fakeMo2 + "/profiles/solero";
+    const QString dataDir  = gameDir + "/Data";
+
+    if (!QDir().mkpath(modsDir)) return fail("Could not create " + modsDir);
+    if (!QDir().mkpath(profDir)) return fail("Could not create " + profDir);
+
+    QStringList order;
+    const PluginList& pl = profile.pluginList();
+    for (int i = 0; i < pl.count(); ++i) {
+        const PluginEntry& e = pl.at(i);
+        if (e.enabled) order << e.filename;
+    }
+    const QByteArray loadorder =
+        (order.join('\n') + (order.isEmpty() ? "" : "\n")).toUtf8();
+    if (!atomicWrite(profDir + "/loadorder.txt", loadorder))
+        return fail("Could not write loadorder.txt");
+
+    QStringList bsas = QDir(dataDir).entryList(QStringList{"*.bsa"},
+                                               QDir::Files, QDir::Name);
+    const QByteArray archives =
+        (bsas.join('\n') + (bsas.isEmpty() ? "" : "\n")).toUtf8();
+    if (!atomicWrite(profDir + "/archives.txt", archives))
+        return fail("Could not write archives.txt");
+
+    if (!atomicWrite(profDir + "/modlist.txt", QByteArray()))
+        return fail("Could not write modlist.txt");
+
+    QJsonObject obj;
+    {
+        QFile f(settingsPath);
+        if (f.open(QIODevice::ReadOnly))
+            obj = QJsonDocument::fromJson(f.readAll()).object();
+    }
+    obj["manual_mode"] = true;
+    obj["game"]          = "SkyrimSE";
+    obj["mo2_folder"]    = fakeMo2;
+    obj["selected_profile"] = "solero";
+    obj["profile_path"]  = profDir;
+    obj["mods_path"]     = modsDir;
+    obj["data_path"]     = dataDir;
+    obj["output_path"]   = outputDataDir;
+
+    if (!QDir().mkpath(QFileInfo(settingsPath).path()))
+        return fail("Could not create config dir for " + settingsPath);
+    if (!atomicWrite(settingsPath, QJsonDocument(obj).toJson(QJsonDocument::Indented)))
+        return fail("Could not write " + settingsPath);
+
+    return true;
+}
+
+} // namespace RadiumPrep
+} // namespace solero
---
+++ tests/test_RadiumPrep.cpp
@@ -0,0 +1,131 @@
+#include <QtTest>
+#include <QTemporaryDir>
+#include <QDir>
+#include <QFile>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include "tools/RadiumPrep.h"
+#include "core/Profile.h"
+#include "core/Types.h"
+using namespace solero;
+
+class TestRadiumPrep : public QObject {
+    Q_OBJECT
+
+    static QString readAll(const QString& path) {
+        QFile f(path);
+        if (!f.open(QIODevice::ReadOnly)) return {};
+        return QString::fromUtf8(f.readAll());
+    }
+    static QJsonObject readJson(const QString& path) {
+        QFile f(path);
+        if (!f.open(QIODevice::ReadOnly)) return {};
+        return QJsonDocument::fromJson(f.readAll()).object();
+    }
+    static void seedPlugins(Profile& p) {
+        PluginEntry a; a.filename = "Skyrim.esm";  a.enabled = true;  a.isMaster = true;
+        PluginEntry b; b.filename = "SkyUI.esp";   b.enabled = true;
+        PluginEntry c; c.filename = "Disabled.esp"; c.enabled = false;
+        p.pluginList().append(a);
+        p.pluginList().append(b);
+        p.pluginList().append(c);
+    }
+    static void touch(const QString& path) {
+        QDir().mkpath(QFileInfo(path).path());
+        QFile f(path); f.open(QIODevice::WriteOnly); f.write("x"); f.close();
+    }
+
+private slots:
+    void writesFakeMo2AndSettings() {
+        QTemporaryDir tmp;
+        const QString root = tmp.path();
+        Profile p("ASSOS", root + "/profiles");
+        seedPlugins(p);
+
+        const QString gameDir = root + "/game";
+        touch(gameDir + "/Data/ZZZ.bsa");
+        touch(gameDir + "/Data/AAA.bsa");
+        touch(gameDir + "/Data/textures/note.txt");
+
+        const QString installDir = root + "/tools/radium";
+        QDir().mkpath(installDir);
+        const QString outDataDir = root + "/staging/Radium Output/Data";
+        const QString settings = root + "/cfg/settings.json";
+
+        QString err;
+        QVERIFY2(RadiumPrep::prepare(p, gameDir, installDir, outDataDir, settings, &err),
+                 qPrintable(err));
+
+        const QString prof = installDir + "/fake-mo2/profiles/solero";
+        QVERIFY(QDir(installDir + "/fake-mo2/mods").exists());
+        QVERIFY(QDir(installDir + "/fake-mo2/mods").entryList(QDir::NoDotAndDotDot | QDir::AllEntries).isEmpty());
+        QCOMPARE(readAll(prof + "/loadorder.txt"), QString("Skyrim.esm\nSkyUI.esp\n"));
+        QCOMPARE(readAll(prof + "/archives.txt"), QString("AAA.bsa\nZZZ.bsa\n"));
+        QCOMPARE(readAll(prof + "/modlist.txt"), QString(""));
+
+        QJsonObject o = readJson(settings);
+        QCOMPARE(o["manual_mode"].toBool(), true);
+        QCOMPARE(o["game"].toString(), QString("SkyrimSE"));
+        QCOMPARE(o["profile_path"].toString(), prof);
+        QCOMPARE(o["mods_path"].toString(), installDir + "/fake-mo2/mods");
+        QCOMPARE(o["data_path"].toString(), gameDir + "/Data");
+        QCOMPARE(o["output_path"].toString(), outDataDir);
+    }
+
+    void preservesUserTuning() {
+        QTemporaryDir tmp;
+        const QString root = tmp.path();
+        Profile p("ASSOS", root + "/profiles");
+        seedPlugins(p);
+
+        const QString settings = root + "/cfg/settings.json";
+        QDir().mkpath(root + "/cfg");
+        {
+            QJsonObject pre;
+            pre["preset"] = "Performance";
+            pre["thread_count"] = 8;
+            pre["use_gpu"] = false;
+            pre["data_path"] = "/old/wrong/path";
+            QFile f(settings); f.open(QIODevice::WriteOnly);
+            f.write(QJsonDocument(pre).toJson()); f.close();
+        }
+
+        const QString gameDir = root + "/game";
+        QDir().mkpath(gameDir + "/Data");
+        const QString installDir = root + "/tools/radium";
+        const QString outDataDir = root + "/staging/Radium Output/Data";
+
+        QString err;
+        QVERIFY2(RadiumPrep::prepare(p, gameDir, installDir, outDataDir, settings, &err),
+                 qPrintable(err));
+
+        QJsonObject o = readJson(settings);
+        QCOMPARE(o["preset"].toString(), QString("Performance"));
+        QCOMPARE(o["thread_count"].toInt(), 8);
+        QCOMPARE(o["use_gpu"].toBool(), false);
+        QCOMPARE(o["data_path"].toString(), gameDir + "/Data");
+        QCOMPARE(o["manual_mode"].toBool(), true);
+    }
+
+    void idempotentAndEmptyData() {
+        QTemporaryDir tmp;
+        const QString root = tmp.path();
+        Profile p("ASSOS", root + "/profiles");
+        seedPlugins(p);
+        const QString gameDir = root + "/game";
+        QDir().mkpath(gameDir + "/Data");
+        const QString installDir = root + "/tools/radium";
+        const QString outDataDir = root + "/staging/Radium Output/Data";
+        const QString settings = root + "/cfg/settings.json";
+
+        QString err;
+        QVERIFY(RadiumPrep::prepare(p, gameDir, installDir, outDataDir, settings, &err));
+        const QString first = readAll(settings);
+        QVERIFY(RadiumPrep::prepare(p, gameDir, installDir, outDataDir, settings, &err));
+        QCOMPARE(readAll(settings), first);
+        QCOMPARE(readAll(installDir + "/fake-mo2/profiles/solero/archives.txt"), QString(""));
+    }
+};
+
+QTEST_MAIN(TestRadiumPrep)
+#include "test_RadiumPrep.moc"
